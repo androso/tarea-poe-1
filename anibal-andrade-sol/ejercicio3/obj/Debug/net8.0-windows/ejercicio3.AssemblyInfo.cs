177e0c11ce25940660754d606dfe8a586491b127
androso <androso421@gmail.com>
2024-08-15 17:00:02 -0600

    renaming directory for exercise name

--- anibal-andrade-sol/ejercicio3/obj/Debug/net8.0-windows/ejercicio3.AssemblyInfo.cs
+++ anibal-andrade-sol/ejercicio3/obj/Debug/net8.0-windows/ejercicio3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejercicio3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab4ecc8aa14f7f0848c175d969c3a115f612085")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1921a4dbe12f7b44e6a11a3f1c06dc8a4d6154a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejercicio3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejercicio3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
